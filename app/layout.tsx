d4b4e597dd7e36e2970ad21a2c3a0e994a9b4df6
Wali <humaidwali20@gmail.com>
2025-08-02 00:08:19 +0530

    lenis

--- app/layout.tsx
+++ app/layout.tsx
@@ -54,15 +54,17 @@ export default function RootLayout({
           storageKey="theme"
           defaultTheme="system"
         >
-          <div className="flex min-h-screen w-full flex-col font-[family-name:var(--font-inter-tight)]">
-            <div className="relative mx-auto w-full max-w-screen-sm flex-1 px-4 pt-20">
-              <Header />
-              {children}
-              <SpeedInsights/>
-              <Analytics />
-              <Footer />
+          <LenisProvider>
+            <div className="flex min-h-screen w-full flex-col font-[family-name:var(--font-inter-tight)]">
+              <div className="relative mx-auto w-full max-w-screen-sm flex-1 px-4 pt-20">
+                <Header />
+                {children}
+                <SpeedInsights/>
+                <Analytics />
+                <Footer />
+              </div>
             </div>
-          </div>
+          </LenisProvider>
         </ThemeProvider>
       </body>
     </html>
